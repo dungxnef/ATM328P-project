34cabe0715d0c26d34c3bf0cfc547974f1e1ba06
dungxnef <105290019+dungxnef@users.noreply.github.com>
2023-10-05 02:10:05 +0700

    Update main.cpp

--- EXERCISE_2_SIMPLE CALCULATOR/main.cpp
+++ EXERCISE_2_SIMPLE CALCULATOR/main.cpp
@@ -22,10 +22,10 @@
 
 // Keypad layout
 unsigned char keypad[4][4] = {
-{'7', '8', '9', '+'},
-{'4', '5', '6', '-'},
-{'1', '2', '3', '*'},
-{'C', '0', '=', '/'}
+	{'7', '8', '9', '+'},
+	{'4', '5', '6', '-'},
+	{'1', '2', '3', '*'},
+	{'C', '0', '=', '/'}
 };
 
 char input_buffer[17]; //  store keypad input (up to 16 characters)
@@ -35,76 +35,76 @@ int result;
 unsigned char colloc, rowloc;
 
 char keyfind(){
-while (1)
-{
-	KEY_DDR = 0xF0; /* set port direction as input-output */
-	KEY_PRT = 0x0F;
-	do
+	while (1)
 	{
-		KEY_PRT &= 0x0F; /* mask PORT for column read only */
-		asm("NOP");
-		colloc = (KEY_PIN & 0x0F); /* read status of column */
-	} while (colloc != 0x0F);
-
-	do
-	{
+		KEY_DDR = 0xF0; /* set port direction as input-output */
+		KEY_PRT = 0x0F;
 		do
 		{
-			_delay_ms(20);             /* 20ms key debounce time */
+			KEY_PRT &= 0x0F; /* mask PORT for column read only */
+			asm("NOP");
 			colloc = (KEY_PIN & 0x0F); /* read status of column */
-			} while (colloc == 0x0F);        /* check for any key press */
+		} while (colloc != 0x0F);
 
-			_delay_ms(20);              /* 20 ms key debounce time */
+		do
+		{
+			do
+			{
+				_delay_ms(20);             /* 20ms key debounce time */
+				colloc = (KEY_PIN & 0x0F); /* read status of column */
+				} while (colloc == 0x0F);        /* check for any key press */
+
+				_delay_ms(20);              /* 20 ms key debounce time */
+				colloc = (KEY_PIN & 0x0F);
+			} while (colloc == 0x0F);
+
+			/* now check for rows */
+			KEY_PRT = 0xEF;             /* check for pressed key in 1st row */
+			asm("NOP");
 			colloc = (KEY_PIN & 0x0F);
-		} while (colloc == 0x0F);
+			if (colloc != 0x0F)
+			{
+				rowloc = 0;
+				break;
+			}
 
-		/* now check for rows */
-		KEY_PRT = 0xEF;             /* check for pressed key in 1st row */
-		asm("NOP");
-		colloc = (KEY_PIN & 0x0F);
-		if (colloc != 0x0F)
-		{
-			rowloc = 0;
-			break;
+			KEY_PRT = 0xDF;             /* check for pressed key in 2nd row */
+			asm("NOP");
+			colloc = (KEY_PIN & 0x0F);
+			if (colloc != 0x0F)
+			{
+				rowloc = 1;
+				break;
+			}
+
+			KEY_PRT = 0xBF;             /* check for pressed key in 3rd row */
+			asm("NOP");
+			colloc = (KEY_PIN & 0x0F);
+			if (colloc != 0x0F)
+			{
+				rowloc = 2;
+				break;
+			}
+
+			KEY_PRT = 0x7F;             /* check for pressed key in 4th row */
+			asm("NOP");
+			colloc = (KEY_PIN & 0x0F);
+			if (colloc != 0x0F)
+			{
+				rowloc = 3;
+				break;
+			}
 		}
 
-		KEY_PRT = 0xDF;             /* check for pressed key in 2nd row */
-		asm("NOP");
-		colloc = (KEY_PIN & 0x0F);
-		if (colloc != 0x0F)
-		{
-			rowloc = 1;
-			break;
-		}
-
-		KEY_PRT = 0xBF;             /* check for pressed key in 3rd row */
-		asm("NOP");
-		colloc = (KEY_PIN & 0x0F);
-		if (colloc != 0x0F)
-		{
-			rowloc = 2;
-			break;
-		}
-
-		KEY_PRT = 0x7F;             /* check for pressed key in 4th row */
-		asm("NOP");
-		colloc = (KEY_PIN & 0x0F);
-		if (colloc != 0x0F)
-		{
-			rowloc = 3;
-			break;
-		}
+		if (colloc == 0x0E)
+		return (keypad[rowloc][0]);
+		else if (colloc == 0x0D)
+		return (keypad[rowloc][1]);
+		else if (colloc == 0x0B)
+		return (keypad[rowloc][2]);
+		else
+		return (keypad[rowloc][3]);
 	}
-
-	if (colloc == 0x0E)
-	return (keypad[rowloc][0]);
-	else if (colloc == 0x0D)
-	return (keypad[rowloc][1]);
-	else if (colloc == 0x0B)
-	return (keypad[rowloc][2]);
-	else
-	return (keypad[rowloc][3]);
-}
 
 
 void LCD_Command(unsigned char cmnd)
@@ -167,147 +167,148 @@ void LCD_Clear()
 }
 
 void Oflow_err_msg(){
-LCD_Clear();
-LCD_String("Overflow Error!");
-input_buffer[0] = '\0'; // Reset the input buffer
+	LCD_Clear();
+	LCD_String("Overflow Error!");
+	input_buffer[0] = '\0'; // Reset the input buffer
 }
 
 void Uflow_error_msg(){
-LCD_Clear();
-LCD_String("Underflow Error!");
-input_buffer[0] = '\0'; // Reset the input buffer
+	LCD_Clear();
+	LCD_String("Underflow Error!");
+	input_buffer[0] = '\0'; // Reset the input buffer
 }
-
 
 int eval_expression(const char *expression, int *result)
 {
-int num = 0;
-int temp_result = 0;
-char op = '+';
-int sign = 1;
-int stack[100], top = -1;
+	int num = 0;
+	int temp_result = 0;
+	char op = '+';
+	int sign = 1;
+	int stack[100], top = -1;
 
-for (int i = 0; expression[i] != '\0'; i++)
-{
-	char current_char = expression[i];
-	if (isdigit(current_char))
+	for (int i = 0; expression[i] != '\0'; i++)
 	{
-		int digit = (current_char - '0') * sign;
-		if ((sign == 1 && num > (INT_MAX - digit) / 10) || (sign == -1 && num < (INT_MIN - digit) / 10)) {
-			return 0;
+		char current_char = expression[i];
+		if (isdigit(current_char))
+		{
+			int digit = (current_char - '0') * sign;
+			if ((sign == 1 && num > (INT_MAX - digit) / 10) || (sign == -1 && num < (INT_MIN - digit) / 10)) {
+				return 0;
+			}
+			num = num * 10 + digit;
 		}
-		num = num * 10 + digit;
+
+		else
+		{
+			switch (op)
+			{
+				case '+':
+				if (num > 0 && stack[top] > INT_MAX - num) {
+					Oflow_err_msg();
+				}
+				else if (num < 0 && stack[top] < INT_MIN - num) {
+					Uflow_error_msg();
+				}
+				stack[++top] = num;
+				break;
+				case '-':
+				if (num < 0 && stack[top] > INT_MAX + num) {
+					Oflow_err_msg();
+				}
+				else if (num > 0 && stack[top] < INT_MIN + num) {
+					Uflow_error_msg();
+				}
+				stack[++top] = -num;
+				break;
+				case '*':
+				if ((stack[top] > 0 && num > INT_MAX / stack[top]) || (stack[top] < 0 && num < INT_MAX / stack[top])) {
+					Oflow_err_msg();
+				}
+				else if ((stack[top] > 0 && num < INT_MIN / stack[top]) || (stack[top] < 0 && num > INT_MIN / stack[top])) {
+					Uflow_error_msg();
+				}
+				stack[top] *= num;
+				break;
+				case '/':
+				// Check for division by zero
+				if (num == 0) {
+					return 0; // Division by zero error
+				}
+				stack[top] /= num;
+				break;
+				default:
+				return 0; // Invalid operator
+			}
+
+			num = 0; // Reset the number
+			op = current_char; // Set the current operator
+
+			if (expression[i + 1] == '-')
+			{
+				sign = -1;
+				i++;
+			}
+			else{
+				sign =1;
+			}
+		}
 	}
 
-	else
+	switch (op)
 	{
-		switch (op)
-		{
-			case '+':
-			if (num > 0 && stack[top] > INT_MAX - num) {
-				Oflow_err_msg();
-			}
-			else if (num < 0 && stack[top] < INT_MIN - num) {
-				Uflow_error_msg();
-			}
-			stack[++top] = num;
-			break;
-			case '-':
-			if (num < 0 && stack[top] > INT_MAX + num) {
-				Oflow_err_msg();
-			}
-			else if (num > 0 && stack[top] < INT_MIN + num) {
-				Uflow_error_msg();
-			}
-			stack[++top] = -num;
-			break;
-			case '*':
-			if ((stack[top] > 0 && num > INT_MAX / stack[top]) || (stack[top] < 0 && num < INT_MAX / stack[top])) {
-				Oflow_err_msg();
-			}
-			else if ((stack[top] > 0 && num < INT_MIN / stack[top]) || (stack[top] < 0 && num > INT_MIN / stack[top])) {
-				Uflow_error_msg();
-			}
-			stack[top] *= num;
-			break;
-			case '/':
-			// Check for division by zero
-			if (num == 0) {
-				return 0; // Division by zero error
-			}
-			stack[top] /= num;
-			break;
-			default:
-			return 0; // Invalid operator
+		case '+':
+		if (num > 0 && stack[top] > INT_MAX - num) {
+			Oflow_err_msg();
+			return -1;
 		}
-
-		num = 0; // Reset the number
-		op = current_char; // Set the current operator
-
-		if (expression[i + 1] == '-')
-		{
-			sign = -1;
-			i++;
+		else if (num < 0 && stack[top] < INT_MIN - num) {
+			Uflow_error_msg();
+			return -2;
 		}
+		stack[++top] = num;
+		break;
+		case '-':
+		if (num < 0 && stack[top] > INT_MAX + num) {
+			Oflow_err_msg();
+			return -1;
+		}
+		else if (num > 0 && stack[top] < INT_MIN + num) {
+			Uflow_error_msg();
+			return -2;
+		}
+		stack[++top] = -num;
+		break;
+		case '*':
+		if ((stack[top] > 0 && num > INT_MAX / stack[top]) || (stack[top] < 0 && num < INT_MAX / stack[top])) {
+			Oflow_err_msg();
+			return -1;
+		}
+		else if ((stack[top] > 0 && num < INT_MIN / stack[top]) || (stack[top] < 0 && num > INT_MIN / stack[top])) {
+			Uflow_error_msg();
+			return -2;
+		}
+		stack[top] *= num;
+		break;
+		case '/':
+		// Check for division by zero
+		if (num == 0) {
+			return 0; // Division by zero error
+		}
+		stack[top] /= num;
+		break;
+		default:
+		return 0; // Invalid operator
 	}
-}
 
-switch (op)
-{
-	case '+':
-	if (num > 0 && stack[top] > INT_MAX - num) {
-		Oflow_err_msg();
-		return -1;
+	while (top != -1)
+	{
+		temp_result += stack[top--];
 	}
-	else if (num < 0 && stack[top] < INT_MIN - num) {
-		Uflow_error_msg();
-		return -2;
-	}
-	stack[++top] = num;
-	break;
-	case '-':
-	if (num < 0 && stack[top] > INT_MAX + num) {
-		Oflow_err_msg();
-		return -1;
-	}
-	else if (num > 0 && stack[top] < INT_MIN + num) {
-		Uflow_error_msg();
-		return -2;
-	}
-	stack[++top] = -num;
-	break;
-	case '*':
-	if ((stack[top] > 0 && num > INT_MAX / stack[top]) || (stack[top] < 0 && num < INT_MAX / stack[top])) {
-		Oflow_err_msg();
-		return -1;
-	}
-	else if ((stack[top] > 0 && num < INT_MIN / stack[top]) || (stack[top] < 0 && num > INT_MIN / stack[top])) {
-		Uflow_error_msg();
-		return -2;
-	}
-	stack[top] *= num;
-	break;
-	case '/':
-	// Check for division by zero
-	if (num == 0) {
-		return 0; // Division by zero error
-	}
-	stack[top] /= num;
-	break;
-	default:
-	return 0; // Invalid operator
-}
-
-while (top != -1)
-{
-	temp_result += stack[top--];
-}
 
 	*result = temp_result;
 
-return 1; // Successful evaluation
+	return 1; // Successful evaluation
 }
-
 
 int main(void) {
 
@@ -317,7 +318,7 @@ int main(void) {
 	while(1)
 	{
 		char key = keyfind();
-		
+			
 		if (key != ' ')
 		{
 			if (isdigit(key))
@@ -380,7 +381,7 @@ int main(void) {
 						input_buffer[0] = '\0'; // Reset the input buffer
 							
 					}
-					
+						
 				}
 			}
 			else if (key == 'C') // Clear the input buffer
